71f8008cae8ba4fca5febd87ce7476de29c36e2f
akims_hfn9x6a <akym.slukhai.25@kse.org.ua>
2026-03-05 20:24:41 +0200

    Added class UniversityMember.cs

---
+++ 00p-week5/UniversityMember.cs
@@ -0,0 +1,37 @@
+﻿namespace UniversityResourceSystem;
+
+public class UniversityMember
+{
+    private string _name;
+
+    private string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("Ім'я не може бути пустим");
+            Name = _name;
+        }
+        
+    }
+
+    public string MemberId { get; }
+
+    private protected List<string> ActionLog = new List<string>();
+
+    public UniversityMember(string name, string id)
+    {
+        if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(id))
+            throw new ArgumentException("Помилка. Проблема з даними.");
+        Name = name;
+        MemberId = id;
+    }
+
+    public virtual void PerformDuties()
+    {
+        if (ActionLog.Count > 5)
+        {
+            throw new InvalidOperationException($"Студент {Name}, {MemberId} досягнув денного обмеження в 5 взаємодій");
+        }
+    }
+}
